5d95e2a01e5377d52509eefc118edf29d6387e2d
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-01-09 14:58:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HeaderComics from './components/HeaderComics.vue';
+import MainComics from './components/MainComics.vue';
+import FooterComics from './components/FooterComics.vue';
+</script>
+
+<template>
+<header>
+  <HeaderComics />
+</header>
+
+<main>
+  <MainComics />
+</main>
+
+<footer>
+  <FooterComics />
+</footer>
+
+</template>
+
+<style scoped>
+
+</style>
